5ec5faa374384d595a848670761ed96ed93531ec
Programmer <aceraspire315510@gmail.com>
2025-01-09 10:45:16 +0500

    version 1.7

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -2,7 +2,7 @@ from . import admin
 from . import text_advertising
 from . import image_advertising
 from . import video_advertising
-from . import add_channel
 from . import delete_channel
 from . import back_buttons
 from . import channels
+from . import add_channel
